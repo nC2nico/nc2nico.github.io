f5532a44f4efa575f0090942e952806edad3246f
nC2nico <93074481+nC2nico@users.noreply.github.com>
2021-10-25 17:48:22 +0900

    Create jsgame01main.js

---
+++ jsgame01main.js
@@ -0,0 +1,10 @@
+var scenenum = 1; //1=title 2=home 3=battle 4=continue 99=error
+
+var mhp = 100;//my hp
+var mat =  10;//my attack
+var mdf =  10;//my defence
+var ehp = 100;//enemy hp
+var eat =  10;//enemy attack
+var edf =  10;//enemy defence
+
+var  level = 1;//my level
